2e34fbef1aa7c1a009e6ad7cbe43165731bd4b9f
Dmytro Roshchupkin <acc.rivers@gmail.com>
2025-06-24 10:26:32 +0200

    Convert to Typescript

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# scrappy-apple
+# Scrappy
+
 Scraps AppStore reviews for a specified app by AppId
+
+To run, make sure Node.js is installed, then run:
+
+```
+npm i && npm start
+```
+
+This project uses TypeScript. The entry point is `index.ts`.
--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 // Pulls AppStore reviews for a specified number of pages for a specifiec appId
 // and saves them to an output text and JSON files (ChatGPT seems to handle JSON better)
 
-// To run, make sure node is istalled, then run "npm i && npm start"
+// To run, make sure node is installed, then run "npm i && npm start"
 // n.b.: Make sure package.json file is in the same folder
 
-const store = require("app-store-scraper")
-const fs = require("fs")
+// @ts-expect-error: No type definitions for 'app-store-scraper'
+import store from "app-store-scraper"
+import fs from "fs"
 
 const PREPLY_ID = "1352790442" // from AppStore url parameters
 const PREPLY_APP_NAME = "preply" // any name, used for output labelling
@@ -19,10 +20,22 @@ const CAMBLY_APP_NAME = "cambly"
 const VERBLING_ID = "1170152447"
 const VERBLING_APP_NAME = "verbling"
 
-const APP_ID = VERBLING_ID
-const APP_NAME = VERBLING_APP_NAME
+const APP_ID: string = VERBLING_ID
+const APP_NAME: string = VERBLING_APP_NAME
 
-const PAGES_COUNT = 10 // there are 50 reviews per page,
+const PAGES_COUNT: number = 10 // there are 50 reviews per page
+
+type Review = {
+  id: string
+  userName: string
+  userUrl: string
+  version: string
+  score: number
+  title: string
+  text: string
+  url: string
+  updated: string
+}
 
 Promise.all(
   [...Array(PAGES_COUNT).keys()].map(page =>
@@ -33,8 +46,8 @@ Promise.all(
     })
   )
 )
-  .then(rawResults => {
-    const results = rawResults.flat()
+  .then((rawResults: Review[][]) => {
+    const results: Review[] = rawResults.flat()
 
     const text_reslts = results.reduce(
       (output, currentReview) =>
@@ -44,7 +57,7 @@ Promise.all(
       ""
     )
 
-    // txt files are conveniet for text search and human analysis
+    // txt files are convenient for text search and human analysis
     fs.writeFile(`${APP_NAME}-appstore-reviews.txt`, text_reslts, err => {
       if (err) throw err
       console.log("Reviews saved as text.")
